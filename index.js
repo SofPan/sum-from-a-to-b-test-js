7f4e53c2bf55b62ef60e2de4c21e9ce169813699
Sofia Panchaud <s.panchaud.89@gmail.com>
2023-11-01 13:16:40 -0400

    added base case to sum

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 
-function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
-}
+// Sum all the values from fromN up to toN
+const sum = (fromN, toN) => {
+  if (fromN === toN) {
+    return fromN;
+  }
+};
 
 module.exports = sum;
